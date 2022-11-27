7b164d516ef63d00df39276650d2580a553068e6
USER <user@USERnoMacBook-Pro.local>
2022-11-28 07:24:08 +0900

    Java変数と定数

---
+++ Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NO = 7;
+		String lang = "眞蒜";
+		System.out.println(lang);
+
+		lang = "さるも木から落ちる";
+		System.out.println(lang);
+
+		System.out.println(LUCKY_NO);
+	}
+}
